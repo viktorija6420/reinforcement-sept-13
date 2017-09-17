a2fa76466ba5e2a86fdb90cf0f26994d1b719396
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-09-17 13:28:19 -0400

    max_by fix

---
+++ exercise.rb
@@ -0,0 +1,15 @@
+# Currently it doesn't work: it gives the record with 10 (belonging
+ # 'someone') points when it should give the record with 50 points (
+ # belonging to 'someone else').
+
+records = [
+  {name: 'someone', points: 10, notes: ["this is a note", "this is too"]},
+  {name: 'someone else', points: 50, notes: ['wow']},
+  {name: 'another person', points: 30, notes: []}
+]
+
+def find_record_with_most_points(records)
+  return records.max_by { |record| record[:points] }
+end
+
+puts find_record_with_most_points(records)
